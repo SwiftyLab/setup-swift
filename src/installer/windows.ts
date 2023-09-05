80ba175b327d2ccb52166a29a56baed87b30cf2c
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2023-09-05 21:17:32 +0530

    fix: fixed swift package build failure on windows (#23)

--- src/installer/windows.ts
+++ src/installer/windows.ts
@@ -1,9 +1,7 @@
-import * as os from 'os'
 import * as path from 'path'
 import {promises as fs} from 'fs'
 import * as core from '@actions/core'
 import {exec} from '@actions/exec'
-import * as semver from 'semver'
 import {VerifyingToolchainInstaller} from './verify'
 import {WindowsToolchainSnapshot} from '../snapshot'
 import {
@@ -17,17 +15,13 @@ export class WindowsToolchainInstaller extends VerifyingToolchainInstaller<Windo
   private visualStudio?: VisualStudio
 
   protected async download() {
-    const reccommended = '10.0.17763'
-    const current = os.release()
-    const version = semver.gte(current, reccommended) ? current : reccommended
-    const winsdk = semver.patch(version)
     const vsRequirement: VisualStudioRequirement = {
       version: '16',
       components: [
+        'Microsoft.VisualStudio.Component.VC.ATL',
+        'Microsoft.VisualStudio.Component.VC.CMake.Project',
         'Microsoft.VisualStudio.Component.VC.Tools.x86.x64',
-        `Microsoft.VisualStudio.Component.Windows10SDK.${winsdk}`,
-        'Component.CPython.x64',
-        'Microsoft.VisualStudio.Component.VC.CMake.Project'
+        'Microsoft.VisualStudio.Component.Windows10SDK'
       ]
     }
     core.debug(`Using Visual Studio requirement ${vsRequirement}`)
@@ -74,9 +68,7 @@ export class WindowsToolchainInstaller extends VerifyingToolchainInstaller<Windo
     if (!this.visualStudio) {
       throw new Error('No supported Visual Studio installation in installer')
     }
-    if (this.version && semver.lt(this.version, '5.4.2')) {
-      await setupSupportFiles(this.visualStudio)
-    }
+    await setupSupportFiles(this.visualStudio, installation.sdkroot)
     const swiftFlags = `-sdk %SDKROOT% -I %SDKROOT%/usr/lib/swift -L %SDKROOT%/usr/lib/swift/windows`
     core.exportVariable('SWIFTFLAGS', swiftFlags)
   }
@@ -112,7 +104,14 @@ class Installation {
     try {
       core.debug(`Checking for development snapshot installation`)
       toolchain = path.join(location, 'Toolchains', '0.0.0+Asserts')
-      sdkroot = path.join(location, 'Toolchains', '0.0.0+Asserts')
+      sdkroot = path.join(
+        location,
+        'Platforms',
+        'Windows.platform',
+        'Developer',
+        'SDKs',
+        'Windows.sdk'
+      )
       runtime = path.join(location, 'Runtimes', '0.0.0')
       await fs.access(toolchain)
     } catch (error) {
