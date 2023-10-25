1a4e4a6b384b3577c1b9d043d49df6197317a4ba
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2023-10-25 16:14:16 +0000

    fix: fixed installation failure for windows toolchain when gpg signature is missing

--- src/installer/verify.ts
+++ src/installer/verify.ts
@@ -7,15 +7,35 @@ import {SignedToolchainSnapshot} from '../snapshot'
 export abstract class VerifyingToolchainInstaller<
   Snapshot extends SignedToolchainSnapshot
 > extends ToolchainInstaller<Snapshot> {
+  private get signatureUrl() {
+    return `${this.baseUrl}/${this.data.download_signature}`
+  }
+
+  private async downloadSignature() {
+    try {
+      return await toolCache.downloadTool(this.signatureUrl)
+    } catch (error) {
+      if (
+        error instanceof toolCache.HTTPError &&
+        error.httpStatusCode === 404
+      ) {
+        return undefined
+      }
+      throw error
+    }
+  }
+
   protected async download() {
     const sigUrl = `${this.baseUrl}/${this.data.download_signature}`
     core.debug(`Downloading snapshot signature from "${sigUrl}"`)
     const [, toolchain, signature] = await Promise.all([
       gpg.setupKeys(),
       super.download(),
-      toolCache.downloadTool(sigUrl)
+      this.downloadSignature()
     ])
-    await gpg.verify(signature, toolchain)
+    if (signature) {
+      await gpg.verify(signature, toolchain)
+    }
     return toolchain
   }
 }
