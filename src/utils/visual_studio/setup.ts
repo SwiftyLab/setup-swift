9f87bba99fb20035b2229259fd76586128844afb
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2025-05-04 08:27:43 +0000

    refactor: updated Visual Studio setup process for required components installation

--- src/utils/visual_studio/setup.ts
+++ src/utils/visual_studio/setup.ts
@@ -1,21 +1,24 @@
+import * as os from 'os'
+import * as path from 'path'
+import {promises as fs} from 'fs'
+import {randomUUID} from 'crypto'
 import * as core from '@actions/core'
 import {exec, getExecOutput} from '@actions/exec'
-import {VisualStudio, VisualStudioRequirement} from './base'
+import {VisualStudio, VisualStudioRequirement, VisualStudioConfig} from './base'
 import {VSWhere} from './vswhere'
 
 declare module './base' {
   // eslint-disable-next-line no-shadow, @typescript-eslint/no-namespace
   export namespace VisualStudio {
+    let shared: VisualStudio | undefined
     function setup(requirement: VisualStudioRequirement): Promise<VisualStudio>
   }
 }
 
-let shared: VisualStudio
-
 /// set up required visual studio tools for swift on windows
 VisualStudio.setup = async function (requirement: VisualStudioRequirement) {
-  if (shared) {
-    return shared
+  if (this.shared) {
+    return this.shared
   }
   /// https://github.com/microsoft/vswhere/wiki/Find-MSBuild
   /// get visual studio properties
@@ -46,16 +49,33 @@ VisualStudio.setup = async function (requirement: VisualStudioRequirement) {
   }
 
   const vsEnv = await vs.env()
-  const comps = requirement.components
+  let comps = requirement.components
   if (
     vsEnv.UCRTVersion &&
     vsEnv.UniversalCRTSdkDir &&
-    vsEnv.VCToolsInstallDir &&
-    comps.length < 3
+    vsEnv.VCToolsInstallDir
   ) {
-    core.debug('VS components already setup, skipping installation')
-    shared = vs
-    return vs
+    const tmpDir = process.env.RUNNER_TEMP || os.tmpdir()
+    const configFileName = `swift-setup-installation-${randomUUID()}.vsconfig`
+    const configPath = path.join(tmpDir, configFileName)
+    core.debug(`Exporting VS installation config to "${configPath}"`)
+    await exec(`"${vs.properties.setupEngineFilePath}"`, [
+      'export',
+      ...vs.defaultOptions,
+      '--config',
+      configPath
+    ])
+
+    const configContent = await fs.readFile(configPath, 'utf-8')
+    core.debug(`Exported configuration data: "${configContent}"`)
+    const vsConfig: VisualStudioConfig = JSON.parse(configContent)
+    const installedComponents = new Set(vsConfig.components)
+    comps = comps.filter(comp => !installedComponents.has(comp))
+    if (comps.length == 0) {
+      core.debug('VS components already setup, skipping installation')
+      this.shared = vs
+      return vs
+    }
   }
 
   /// https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2022
@@ -63,13 +83,11 @@ VisualStudio.setup = async function (requirement: VisualStudioRequirement) {
   core.debug(`Installing VS components "${comps}" at "${vs.installationPath}"`)
   await exec(`"${vs.properties.setupEngineFilePath}"`, [
     'modify',
-    '--installPath',
-    vs.installationPath,
+    ...vs.defaultOptions,
     ...requirement.components.flatMap(component => ['--add', component]),
     '--installWhileDownloading',
-    '--force',
-    '--quiet'
+    '--force'
   ])
-  shared = vs
+  this.shared = vs
   return vs
 }
