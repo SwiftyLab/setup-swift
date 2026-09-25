a8f39fa5a33ac9fd1e6a09026e84c8df3d7c9fc4
Soumya Ranjan Mahunt <soumya.mahunt@gmail.com>
2026-09-25 09:52:20 +0000

    ci: fixed forked repo ci and changelog script

--- .github/changelog/config.cjs
+++ .github/changelog/config.cjs
@@ -1,5 +1,5 @@
 'use strict'
-const config = require('conventional-changelog-conventionalcommits')
+const config = require('conventional-changelog-conventionalcommits').default
 
 module.exports = config({
   types: [
